eaae29ce187e79e170034debd95255719220e8b0
Andy Gallagher <fredex42@gmail.com>
2026-09-08 13:01:40 +0100

    Implement `getCuratedCollection` query

--- api/src/commonMain/kotlin/com/gu/recipe/backend/repository/GraphQLRepository.kt
+++ api/src/commonMain/kotlin/com/gu/recipe/backend/repository/GraphQLRepository.kt
@@ -1,6 +1,7 @@
 package com.gu.recipe.backend.repository
 
 import com.gu.recipe.backend.graphql.GraphQlResult
+import com.gu.recipe.backend.graphql.generated.CuratedContainerByIdQuery
 import com.gu.recipe.backend.graphql.generated.GetDishOfTheDayRecipeQuery
 import com.gu.recipe.backend.graphql.generated.GetFrontsByRegionQuery
 import com.gu.recipe.backend.graphql.generated.type.Editions
@@ -28,4 +29,8 @@ interface GraphQLRepository {
         region: Regions,
         edition: Editions,
     ): GraphQlResult<GetDishOfTheDayRecipeQuery.Container?>
+
+    suspend fun getCuratedCollection(
+        collectionId: String
+    ): GraphQlResult<CuratedContainerByIdQuery.CuratedContainerById?>
 }
--- api/src/commonMain/kotlin/com/gu/recipe/backend/repository/GraphQlRepositoryImpl.kt
+++ api/src/commonMain/kotlin/com/gu/recipe/backend/repository/GraphQlRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.gu.recipe.backend.repository
 
 import com.gu.recipe.backend.graphql.GraphQlResult
+import com.gu.recipe.backend.graphql.GraphQLError
+import com.gu.recipe.backend.graphql.generated.CuratedContainerByIdQuery
 import com.gu.recipe.backend.graphql.generated.GetDishOfTheDayRecipeQuery
 import com.gu.recipe.backend.graphql.generated.GetFrontsByRegionQuery
 import com.gu.recipe.backend.graphql.generated.type.Editions
@@ -32,4 +34,24 @@ internal class GraphQlRepositoryImpl(
             edition = edition,
         )
     }
+
+    override suspend fun getCuratedCollection(
+        collectionId: String
+    ): GraphQlResult<CuratedContainerByIdQuery.CuratedContainerById?> {
+        if (!UUID_REGEX.matches(collectionId)) {
+            return GraphQlResult.Failure(
+                GraphQLError.Unexpected(
+                    IllegalArgumentException("Invalid collectionId UUID: $collectionId")
+                )
+            )
+        }
+
+        return dataSource.getCuratedCollection(
+            collectionId = collectionId,
+        )
+    }
+
 }
+
+private val UUID_REGEX =
+    Regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$")
