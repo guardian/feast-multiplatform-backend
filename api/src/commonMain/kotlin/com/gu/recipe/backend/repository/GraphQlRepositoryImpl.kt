93f0ffa2abb380f0dbff9050c9c9a1bd91ee1836
Shamoon <178160602+shamoon-guardian@users.noreply.github.com>
2026-09-24 17:32:25 +0100

    throwing correct exception IllegalArgumentException

--- api/src/commonMain/kotlin/com/gu/recipe/backend/repository/GraphQlRepositoryImpl.kt
+++ api/src/commonMain/kotlin/com/gu/recipe/backend/repository/GraphQlRepositoryImpl.kt
@@ -1,10 +1,8 @@
 package com.gu.recipe.backend.repository
 
-import com.gu.recipe.backend.exceptions.GraphQLRepositoryException
 import com.gu.recipe.backend.exceptions.cancellationExceptionOrNull
 import com.gu.recipe.backend.exceptions.toRepositoryException
 import com.gu.recipe.backend.graphql.GraphQlResult
-import com.gu.recipe.backend.graphql.GraphQLError
 import com.gu.recipe.backend.graphql.generated.CuratedContainerByIdQuery
 import com.gu.recipe.backend.graphql.generated.GetDishOfTheDayRecipeQuery
 import com.gu.recipe.backend.graphql.generated.GetFrontsByRegionQuery
@@ -33,7 +31,7 @@ internal class GraphQlRepositoryImpl(
         collectionId: String,
     ): CuratedContainerByIdQuery.CuratedContainerById? {
         if (!UUID_REGEX.matches(collectionId)) {
-            throw GraphQLRepositoryException("Invalid collectionId UUID: $collectionId")
+            throw IllegalArgumentException("Invalid collectionId UUID: $collectionId")
         }
 
         return dataSource.getCuratedCollection(collectionId).getOrThrow()
